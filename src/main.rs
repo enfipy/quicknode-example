c7b0a028d9170c1ffb185ab9d72918fb3b3c8c66
David Ackerman <enfipy@gmail.com>
2021-07-25 14:25:48 +0300

    Alter lib

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use bdk::bitcoincore_rpc::{json::EstimateMode, Auth, Client, RpcApi};
+use bitcoincore_rpc::{Auth, Client, RpcApi};
 
 fn main() {
     let rpc_url = std::env::var("RPC_URL").unwrap();
     println!("{:?}", rpc_url);
     let rpc_client = Client::new(rpc_url, Auth::None).unwrap();
-    let fee = rpc_client
-        .estimate_smart_fee(1, Some(EstimateMode::Economical))
-        .unwrap();
-    println!("{:?}", fee);
+    let info = rpc_client.get_blockchain_info().unwrap();
+    println!("Info: {:?}", info);
 }
